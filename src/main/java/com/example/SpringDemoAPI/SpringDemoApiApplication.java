3a1a3e53ba15a07170113571ace6997beb14aa19
siddarthinme <siddarthinme@gmail.com>
2025-04-13 17:55:43 +0530

    Initial commit

---
+++ src/main/java/com/example/SpringDemoAPI/SpringDemoApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringDemoAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringDemoAPI/SpringDemoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringDemoAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
